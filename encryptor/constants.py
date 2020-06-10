09d8d09d6873e07a1c0b66e0f0028e80408b14bd
Szymon Gebler <gebler.szymon@gmail.com>
2020-06-11 01:36:17 +0200

    fix: remove keys and assets

--- encryptor/constants.py
+++ encryptor/constants.py
@@ -8,10 +8,3 @@ PUBLIC_KEY_DIR = "public"
 PRIVATE_KEY_DIR = "private"
 PUBLIC_KEY_PATH = os.path.join(PUBLIC_KEY_DIR, "pubkey.pem")
 PRIVATE_KEY_PATH = os.path.join(PRIVATE_KEY_DIR, "privkey.pem")
-
-# TODO: remove following constants.
-ASSETS = os.path.join(ROOT_DIR, "assets/")
-SEND_PUBLIC_KEY = os.path.join(ASSETS, "send_public.pem")
-SEND_PRIVATE_KEY = os.path.join(ASSETS, "send_private.pem")
-RECEIVE_PUBLIC_KEY = os.path.join(ASSETS, "receive_public.pem")
-RECEIVE_PRIVATE_KEY = os.path.join(ASSETS, "receive_private.pem")
